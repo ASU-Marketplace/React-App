5ea8cdb327320e14141c9437f643f772ba53bbb1
rdgh0st <83750015+rdgh0st@users.noreply.github.com>
2023-02-10 12:22:35 -0700

    fixes to database connection

--- src/server/dbconnection.js
+++ src/server/dbconnection.js
@@ -1,22 +1,25 @@
+require('dotenv').config();
+
 const mysql = require('mysql');
 
 const DB_USER = process.env.DB_USER;
-const DB_PASS = process.env.DB_PASS;
+const DB_PASS_PRODUCTS = process.env.DB_PASS_PRODUCTS;
+const DB_PASS_USERS = process.env.DB_PASS_USERS;
 const DB_NAME_USERS = process.env.DB_NAME_USERS;
 const DB_NAME_PRODUCTS = process.env.DB_NAME_PRODUCTS;
 
-const usersConnection = mysql.createConnection({
+export const usersConnection = mysql.createConnection({
   host: `/cloudsql/${process.env.DB_INSTANCE_USERS}`,
   user: DB_USER,
-  password: DB_PASS,
+  password: DB_PASS_USERS,
   database: DB_NAME_USERS,
   socketPath: `/cloudsql/${process.env.DB_INSTANCE_USERS}`,
 });
 
-const productsConnection = mysql.createConnection({
+export const productsConnection = mysql.createConnection({
   host: `/cloudsql/${process.env.DB_INSTANCE_PRODUCTS}`,
   user: DB_USER,
-  password: DB_PASS,
+  password: DB_PASS_PRODUCTS,
   database: DB_NAME_PRODUCTS,
   socketPath: `/cloudsql/${process.env.DB_INSTANCE_PRODUCTS}`,
 });
