b770b6acf6581bcd8a7eb4adb87dfa5ed8c69c38
Undershot123 <carlos.2000.guerrero@gmail.com>
2023-03-01 21:48:09 -0700

    Update index.js

--- src/pages/createListing/index.js
+++ src/pages/createListing/index.js
@@ -1,3 +1,11 @@
 export function ProductListing(){
-    return <h1>Create Listing</h1>
+    return (
+        <form>
+            <label for="title">First name:</label> 
+            <input type="text" id="title" name="Title"></input><br></br>
+            <label for="description">Last name:</label> 
+            <input type="text" id="description" name="Enter description of item"></input><br></br>
+            <input type="submit" value="Submit"></input> 
+        </form>
+    )
 }
