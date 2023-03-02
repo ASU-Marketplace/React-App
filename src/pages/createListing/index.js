2cf8b1f3652eb2f660f93b626ad65e5532eb5941
Undershot123 <carlos.2000.guerrero@gmail.com>
2023-03-01 22:09:35 -0700

    Updated index.js and css file

--- src/pages/createListing/index.js
+++ src/pages/createListing/index.js
@@ -1,11 +1,22 @@
 export function ProductListing(){
     return (
         <form>
-            <label for="title">First name:</label> 
-            <input type="text" id="title" name="Title"></input><br></br>
-            <label for="description">Last name:</label> 
-            <input type="text" id="description" name="Enter description of item"></input><br></br>
-            <input type="submit" value="Submit"></input> 
+            <label for="title">Title:</label> 
+            <input type="text" id="title" name="title"></input><br></br>
+
+            <label for="description">Description of product:</label> 
+            <input type="text" id="description" name="description"></input><br></br>
+
+            <label for="category">Category*:</label> 
+            <input type="text" id="category" name="category" required></input><br></br>
+
+            <label for="condition">Condition*:</label> 
+            <input type="text" id="condition" name="condition" required></input><br></br>
+
+            <label for="YMM">Year, Make, and/or Model</label> 
+            <input type="text" id="YMM" name="YMM"></input><br></br>
+
+            <input type="submit" value="Submit"></input>
         </form>
     )
 }
