0da8d171eb212592bdad61eac6adcc379970af6b
Undershot123 <carlos.2000.guerrero@gmail.com>
2023-03-19 15:59:00 -0700

    Added template to create personal listings

--- src/pages/createListing/index.js
+++ src/pages/createListing/index.js
@@ -1,22 +1,129 @@
+import React, { useState } from "react";
+import "./styles.css"
+
 export function ProductListing(){
+    const [name, setName] = useState("");
+    const [description, setDesc] = useState("");
+    const [picture, setPicture] = useState(null);
+    const [previewURL, setPreviewURL] = useState(null);
+    const [editMode, setEditMode] = useState(null);
+    const category = ["Apparel", "Technology", "Decor", "Other"];
+    const condition = ["Poor", "Less than average", "Average", "Above average", "Mint"];
+    
+    
+    const handleEdit = () => {
+        setEditMode(true);
+    }
+
+    const handleSave = () => {
+        setEditMode(false);
+    }
+
+    const handlePictureChange = (event) => {
+        setPicture(event.target.files[0]);
+        setPreviewURL(URL.createObjectURL(event.target.files[0]));
+    };
+
+    const [selectedCondition, setSelectedCondition] = useState(condition[0]);
+    const [selectedCategory, setSelectedCategory] = useState(category[0]);
+    const submit = () => {
+        console.log("Selected Condition: " + selectedCondition);
+        console.log("Selected Category: " + selectedCategory);
+    };
+
     return (
-        <form>
-            <label for="title">Title:</label> 
-            <input type="text" id="title" name="title"></input><br></br>
+        <div className="create-listing-container">
+            <div className="form-group">
+                <label htmlFor="picture">Item Picture:</label>
+                <input
+                    className="textInput"
+                    id="picture"
+                    type="file"
+                    accept="image/*"
+                    onChange={handlePictureChange}
+                    disabled={!editMode}
+                />
+                {previewURL && (
+                    <div>
+                        <p>Preview:</p>
+                        <img
+                            src={URL.createObjectURL(picture)}
+                            width="50"
+                            height="50"
+                        />
+                    </div>
+                )}
+            </div>
 
-            <label for="description">Description of product:</label> 
-            <input type="text" id="description" name="description"></input><br></br>
+            <div className="form-group">
+                <label htmlFor="itemName">Item Name:</label>
+                <input
+                    className="textInput"
+                    id="itemName"
+                    type="text"
+                    value={name}
+                    onChange={(edit) => setName(edit.target.value)}
+                    disabled={!editMode}
+                />
+            </div>
 
-            <label for="category">Category*:</label> 
-            <input type="text" id="category" name="category" required></input><br></br>
+            <div className="form-group">
+                <label htmlFor="description">Item Description: </label>
+                <input
+                    className="textInput"
+                    id="description"
+                    type="text"
+                    value={description}
+                    onChange={(edit) => setDesc(edit.target.value)}
+                />
+            </div>
 
-            <label for="condition">Condition*:</label> 
-            <input type="text" id="condition" name="condition" required></input><br></br>
+            <div className="form-group">
+                <label htmlFor="category">Item Category:</label>
+                <select
+                    className="categorySelect"
+                    id="category"
+                    value={selectedCategory}
+                    disabled={!editMode}
+                    onChange={(edit) => setSelectedCategory(edit.target.value)}
+                >
+                    {category.map((value) => (
+                        <option value={value} key={value}>
+                            {value}
+                        </option>
+                    ))}
+                </select>
+            </div>
 
-            <label for="YMM">Year, Make, and/or Model</label> 
-            <input type="text" id="YMM" name="YMM"></input><br></br>
+            <div className="form-group">
+                <label htmlFor="condition">Condition</label>
+                <select
+                    className="conditionSelect"
+                    id="condition"
+                    value={selectedCondition}
+                    disabled={!editMode}
+                    onChange={(edit) => setSelectedCondition(edit.target.value)}
+                >
+                    {condition.map((value) => (
+                        <option value={value} key={value}>
+                            {value}
+                        </option>
+                    ))}
+                </select>
+            </div>
 
-            <input type="submit" value="Submit"></input>
-        </form>
-    )
+            <div className="form-group">
+                {editMode ? (
+                    <button className="btn-save" onClick={handleSave}>
+                        Save
+                    </button>
+                ) : (
+                    <button className="btn-edit" onClick={handleEdit}>
+                        Edit
+                    </button>
+                )}
+            </div>    
+        </div>
+        
+    );
 }
