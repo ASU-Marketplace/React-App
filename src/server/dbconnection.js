2d168786571166d2aa7e69259b36c9fa6ede7c96
rdgh0st <83750015+rdgh0st@users.noreply.github.com>
2023-03-19 15:37:22 -0700

    working write to db, setup for reading

--- src/server/dbconnection.js
+++ src/server/dbconnection.js
@@ -3,7 +3,23 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import useStyles from "../components/products/styles";
 import React from "react";
+import { doc, getDoc, setDoc, deleteField } from 'firebase/firestore';
 
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAyEP-kjqYm84_oixqlwXJXChTyNa0U4m4",
+
+  authDomain: "asu-marketplace-app.firebaseapp.com",
+
+  projectId: "asu-marketplace-app",
+
+  storageBucket: "asu-marketplace-app.appspot.com",
+
+  messagingSenderId: "29170011023",
+
+  appId: "1:29170011023:web:2385558d6b4112fc1bd9fa"
+
+};
 
 
 // Use this to initialize the firebase App
@@ -15,17 +31,22 @@ const auth = firebase.auth();
 
 export { auth, db };
 
-export const addValue = () => {
-  //alert("GETTING HERE");
-  db.collection("values")
-      .doc("value")
-      .set({
-        value: "testing",
-      })
-      .then(function () {
-        alert("Value successfully written!");
+export const addToCartDB = async (collection, value) => {
+    db.collection(collection).add({
+        "Current Cart": value
       })
       .catch(function (error) {
         alert("Error writing Value: " + error);
-      });
+      }); 
 };
+
+export const getValue = async (collection, key) => {
+  const docRef = doc(db, collection, key);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    alert("Document Data:", docSnap.data);
+  } else {
+    alert("No Such Document!");
+  }
+}
