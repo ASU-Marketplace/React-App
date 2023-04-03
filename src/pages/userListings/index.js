c4e6afa273da7b02e1a2bf166e7699bcbff0fe81
Patrick Wojcik <70239167+Pwojcik2@users.noreply.github.com>
2023-04-02 21:24:44 -0700

    sprint 10 changes

--- src/pages/userListings/index.js
+++ src/pages/userListings/index.js
@@ -1,3 +1,19 @@
+import React from 'react';
+import { Container, Typography, Button, Grid } from '@material-ui/core';
+import useStyles from './styles';
+
+
 export function UserListing(){
-    return <h1>Saved Listings</h1>
+    const classes = useStyles();
+    return (
+        <Container>
+                <Typography className={classes.title} variant='h3'>
+                    My Listings
+                </Typography>
+
+                <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary'>
+                    Edit Listings
+                </Button>    
+        </Container>
+    )
 }
